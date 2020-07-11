53b36e10fe9cc5187096f749f346f8ce04d68a9c
Io <io1007@icloud.com>
2020-07-11 18:34:59 +0900

    modified ver

--- pn.cpp
+++ pn.cpp
@@ -1,12 +1,20 @@
 #include <stdio.h>
 int main(){
 	int n;
+	int flag;
 	scanf("%d",&n);
-	printf("%d\n",n);
-	for (int i = 1; i <= n; i++){
-		if((i % 3 != 0) && (i % 2 != 0)){
-			printf("%d\n",i);
+	for (int i = 2; i <= n; i++){
+	   flag = 0;
+	   for(int j = 2; j < i; j++){
+		if(i % j == 0){
+			flag = 1;
+			break;
 		}
+	   }
+	   if(flag == 0){
+		printf("%d\n",i);
+	   }
 	}
+	return 0;
 }
 
